36c48a279c89ec403b9ca6f68f1a5c079d7e6f05
KonogovDenis <konogovdenis@gmail.com>
2026-02-04 12:10:32 +1000

    добавил метрику

--- data_generator.py
+++ data_generator.py
@@ -5,6 +5,7 @@ from prometheus_client import start_http_server, Counter, Gauge
 # Метрики
 ORDERS_TOTAL = Counter('shop_orders_total', 'Total number of orders', ['status', 'product_category'])
 LAST_ORDER_PRICE = Gauge('shop_last_order_price', 'Price of the last processed order')
+REVENUE_TOTAL = Counter('shop_revenue_total', 'Total revenue', ['product_category'])
 
 def generate_data():
     categories = ['Electronics', 'Books', 'Clothing', 'Home']
@@ -17,6 +18,7 @@ def generate_data():
 
         # Записываем данные в метрики
         ORDERS_TOTAL.labels(status=status, product_category=cat).inc()
+        REVENUE_TOTAL.labels(product_category=cat).inc(price)
         LAST_ORDER_PRICE.set(price)
 
         print(f"Generated: {cat} - {status} - ${price:.2f}")
